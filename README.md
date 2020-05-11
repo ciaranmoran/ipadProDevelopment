551685165e40d630b457de4f2b86c5be1bd6fbb9
Ciaran Moran <ciaranm87@ymail.com>
2020-05-11 22:46:37 +1000

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# ipadProDevelopment
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  const a = {
+    plum: "s",
+    plummmy: "bbbbb"
+  };
+
+  const wow = "cooooool";
+
+  console.log(a.plum);
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
